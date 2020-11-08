d1944f7897e745b888c16188d51eae10f55cdc40
AnshulPandey-01 <anshulpandey.indore@gmail.com>
2020-11-08 22:24:23 +0530

    Added other operation
    Previous application only has addition operation now I added subtraction, multiplication and division operation also.

--- MathGame_JS.js
+++ MathGame_JS.js
@@ -1,23 +1,61 @@
 var numOne;
 var numTwo;
+var operation;
 var correctAnswer;
+var counter = 0;
+var best = 0;
+
+var inputField = document.getElementById("answer");
+inputField.addEventListener("keyup", function(event) {
+    if (event.code==="Enter" || event.code==="NumpadEnter") {
+        event.preventDefault();
+        addition();
+        setTimeout(randomExpression, 3000);
+    }
+});
 
 function addition(){
     var userAnswer = document.getElementById("answer").value;
-    if(isNaN(userAnswer)){
-        document.getElementById("output").innerHTML = userAnswer + "is not a number!";
+    var displayField = document.getElementById("output");
+    var streakField = document.getElementById("streak");
+    var bestStreak = document.getElementById("Bstreak");
+    if(isNaN(userAnswer) || userAnswer===""){
+        displayField.innerHTML = userAnswer + " is not a number!";
     }else{
-        if(userAnswer == correctAnswer){
-            document.getElementById("output").innerHTML = "Correct! " + numOne + " + " + numTwo + " = " + correctAnswer;
+        if(userAnswer==correctAnswer){
+            displayField.style.color = "#05bb60";
+            displayField.innerHTML = "Correct! " + correctAnswer;
+            counter++;
+            streakField.innerHTML = "Current Streak: " + counter;
+            if(counter>best){
+                best = counter;
+                bestStreak.innerHTML = "Best Streak: " + best;
+            }
         }else{
-            document.getElementById("output").innerHTML = "Incorrect! " + numOne + " + " + numTwo + " = " + correctAnswer + ", not " + userAnswer;
+            displayField.style.color = "#ff0000";
+            displayField.innerHTML = "Incorrect! " + userAnswer + "<br>" + "Correct Answer: " + correctAnswer;
+            streakField.innerHTML = "Current Streak: " + counter;
+            counter = 0;
         }
     }
 }
 
 function randomExpression(){
-    numOne = Math.floor(Math.random()*10) + 1;
-    numTwo = Math.floor(Math.random()*10) + 1;
-    document.getElementById("expression").innerHTML = numOne + " + " + numTwo;
-    correctAnswer = numOne + numTwo;
+    inputField.value = "";
+    numOne = Math.floor(Math.random()*10);
+    numTwo = Math.floor(Math.random()*9) + 1;
+    operation = Math.floor(Math.random()*4) + 1;
+    if(operation===1){
+        document.getElementById("expression").innerHTML = numOne + " + " + numTwo;
+        correctAnswer = numOne + numTwo;
+    }if(operation===2){
+        document.getElementById("expression").innerHTML = numOne + " - " + numTwo;
+        correctAnswer = numOne - numTwo;
+    }if(operation===3){
+        document.getElementById("expression").innerHTML = numOne + " * " + numTwo;
+        correctAnswer = numOne * numTwo;
+    }if(operation===4){
+        document.getElementById("expression").innerHTML = numOne + " / " + numTwo;
+        correctAnswer = Math.floor(numOne / numTwo);
+    }
 }
